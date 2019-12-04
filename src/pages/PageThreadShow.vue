adcbc29e1298a1b6672725d517efd5ee9affed0e
Patric Poba <poba.dev@outlook.com>
2019-12-04 13:41:04 +0000

    Let the dicsusion begin - End of video 17

--- src/pages/PageThreadShow.vue
+++ src/pages/PageThreadShow.vue
@@ -3,30 +3,16 @@
     <div class="col-large push-top"> 
       <h1>{{ thread.title }}</h1>
 
-      <div class="post-list">
-        <div v-for="postId in thread.posts" class="post">
-          <div class="user-info">
-            <a href="#" class="user-name">{{users[posts[postId].userId].name}}</a>
+       <PostList :posts="posts"/>
 
-            <a href="#">
-              <img class="avatar-large" :src="users[posts[postId].userId].avatar" alt="">
-            </a>   
-
-            <p class="desktop-only text-small">107 posts</p>
-          </div>
-
-          <div class="post-content">
-            <div>
-              {{posts[postId].text}}
-            </div>
-          </div>
-
-          <div class="post-date text-faded">
-            {{posts[postId].publishedAt}}
-          </div>
-        </div>
-      </div>
-      
+       <form @submit.prevent="addPost">
+         <div class="form-group">
+           <textarea v-model="newPostText" cols="30" rows="10" class="form-input"></textarea>
+         </div>
+         <div class="form-actions">
+           <button class="btn-blue">Submit post</button>
+         </div>
+       </form>
     </div>
   </div>
 </template>
@@ -34,10 +20,15 @@
 
 <script>
 import sourceData from '@/data'
+import PostList from '@/components/PostList'
 
 export default {
   name: 'PageThreadShow',
-  // props: ['id'],
+
+  components: {
+    PostList
+  },
+
   props: {
     id: {
       required: true,
@@ -45,11 +36,41 @@ export default {
       default: 'iD'
     }
   },
+
+  computed: {
+    posts () {
+      const postIds = Object.values(this.thread.posts)
+      return Object.values(sourceData.posts)
+        .filter(post => postIds.includes(post['.key']))
+    }
+  },
+
   data () {
     return {
       thread: sourceData.threads[this.id],
-      posts: sourceData.posts,
-      users: sourceData.users
+      newPostText: ''
+    }
+  },
+
+  methods: {
+    addPost () {
+      const postId = 'greatPost' + Math.random()
+      const post = {
+        text: this.newPostText,
+        publishedAt: Math.floor(Date.now() / 1000),
+        threadId: this.id,
+        userId: 'FsCDAk9w8NeXEceLV87arpsXjnQ2',
+        '.key': postId
+      }
+
+      // use the method below to set value to make it react to the changes
+      // this.$set(object, propertyName, value)
+      this.$set(sourceData.posts, postId, post)
+      this.$set(this.thread.posts, postId, postId)
+      // increase user post count
+      this.$set(sourceData.users[post.userId].posts, postId, postId)
+
+      this.newPostText = ''
     }
   }
 }
